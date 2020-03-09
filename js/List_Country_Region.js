e3a74844c06d810bc9ae3d237e80a1fd39888417
nyhoustl <naoki@charter.net>
2020-03-08 22:46:58 -0500

    morocco

--- js/List_Country_Region.js
+++ js/List_Country_Region.js
@@ -1,20 +1,20 @@
+// This is the list of Photogalleries listed by country and region
+// Listed galleries has its own htmls (Gallery_Region)
 country_region_dict = {
   'Cuba': ['Havana', 'Trinidad'],
   'Easter Island': ['Easter Island 1', 'Easter Island 2'],
   'Patagonia':['Perito Moreno Glacier', 'El Chalten'],
   'Mexico': ['San Miguel de Allende', 'Puerto Vallarta'],
-  'USA': ['Arizona', 'California', 'Oregon and Washington',
-          'Minnesota', 'Hawaii'],
-          //'New York'
-  'France': ['Paris', 'Alsace', 'Bordeaux', 'Aveyron',
-            'Normandie', 'Provence', 'Rhone', 'Basque'],
-  'Italy': ['Florence and Siena', "Val d'Orcha 1", "Val d'Orcha 2",'Maremma',
-            'Civita and Orvieto', 'Rome'],
-            //'Milan and Como', 'Ravenna', 'Naples', 'Positano'],
+  'USA': ['Arizona', 'California', 'Oregon and Washington', 'Minnesota', 'Hawaii'],
+  //'New York'
+  'France': ['Alsace', 'Bordeaux', 'Aveyron', 'Normandie', 'Provence', 'Rhone', 'Basque'],
+  //'Paris'
+  'Italy': ['Florence and Siena', "Val d'Orcha 1", "Val d'Orcha 2",'Maremma', 'Civita and Orvieto', 'Rome'],
+  //'Milan and Como', 'Ravenna', 'Naples', 'Positano'
   'Spain': ['Andalucia', 'Barcelona', 'Basque'],
   'UK and Ireland': ['Ireland', 'Scotland'],
-  'Other Countries':['Croatia 1', 'Croatia 2', 'Montenegro', 'Czech', 'Norway', 'Greece',
-            'Portugal', 'Thailand']
+  'Morocco': ['Fes', 'Marrakech', 'Morocco Other'],
+  'Other Countries':['Croatia 1', 'Croatia 2', 'Montenegro', 'Czech', 'Norway', 'Greece', 'Portugal', 'Thailand']
 }
 
 var urlHash = location.hash.slice(1) // obtain keyword from url hash
